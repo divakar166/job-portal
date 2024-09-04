2a6d55b79a5677b9dfe1d8d65803cdebe40d6030
Divakar Singh <ds299555@gmail.com>
2024-09-04 10:07:51 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/backend': {
-        target: 'http://localhost:5000',
+        target: 'https://job-portal-server-mu.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/backend/, ''),
       },
